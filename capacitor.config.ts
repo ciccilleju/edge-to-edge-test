67e87ba5a38a83935eafd984070f0f63df20be98
Francesco De Giorgio <thecoolbreezers@gmail.com>
2025-12-31 15:13:40 +0100

    config edge-to-edge

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'edge-to-edge',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+
+    "EdgeToEdge": {
+      "backgroundColor": "#ffffff",
+      "navigationBarColor": "#f8f9fa",
+      "statusBarColor": "#ff7600"
+    }
+  }
+
+
 };
 
 export default config;
